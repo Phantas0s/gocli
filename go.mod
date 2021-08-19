00dadf0308c0a6c894f772fc91faeaea62a2c0d5
Matthieu <matthieu.cneude@gmail.com>
2021-08-19 19:21:36 +0200

    Simplify everything

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gocli
+
+go 1.16
+
+require github.com/spf13/cobra v1.2.1 // indirect
--- main.go
+++ main.go
@@ -1,23 +1,9 @@
 package main
 
 import (
-	"path/filepath"
-
-	"github.com/Phantas0s/go-cli/cmd"
-	"github.com/adrg/xdg"
-	"github.com/spf13/viper"
+	"github.com/Phantas0s/gocli/cmd"
 )
 
 func main() {
-	initConfig()
 	cmd.Execute()
-}
-
-func initConfig() {
-	viper.AddConfigPath(filepath.Join(xdg.ConfigHome, "go-cli"))
-	viper.AddConfigPath(".")
-	viper.SetConfigName("config")
-
-	viper.AutomaticEnv()
-	viper.ReadInConfig()
 }
